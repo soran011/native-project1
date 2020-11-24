ddadd047f769dc70c42dbaa22bf4a5186ebc9f6f
soran011 <haruti.soran@gmail.com>
2020-11-23 23:23:12 -0500

    built the icon component, so it's ready for me to start building the acount screen

--- App.js
+++ App.js
@@ -7,9 +7,14 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from './app/components/Screen'
+import Icon from './app/components/Icon'
+import ListItem from "./app/components/ListItem";
 
 export default function App() {
   return (
-  <MessagesScreen />
+  <Screen>
+    <ListItem title="my title"  ImageComponent={<Icon name="email"/>}/>
+  </Screen>
   );
 }
